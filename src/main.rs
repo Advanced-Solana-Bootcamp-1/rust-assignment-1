f745d47b236336b2ce706729b4e324f6dc0ed665
Uygun Bodur <uygun@dop.com.tr>
2024-02-15 20:01:26 +0300

    answer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+enum Publication {
+    Book(Book),
+    Magazine(Magazine),
+}
+
+struct Book {
+    title: String,
+    author: String,
+    page_count: u32,
+}
+
+struct Magazine {
+    title: String,
+    issue: u32,
+    topic: String,
+}
+
+fn print_publications(publications: Vec<Publication>) {
+    for publication in publications {
+        match publication {
+            Publication::Book(ref book) => {
+                println!(
+                    "Book: {} author: {}, {} pages",
+                    book.title, book.author, book.page_count
+                );
+            }
+            Publication::Magazine(ref magazine) => {
+                println!(
+                    "Magazine: {} - Issue: {}, Topic: {}",
+                    magazine.title, magazine.issue, magazine.topic
+                );
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let book1 = Book {
+        title: "The Rust Programming Language".to_string(),
+        author: "K. Rustacean".to_string(),
+        page_count: 320,
+    };
+
+    let magazine1 = Magazine {
+        title: "Rust World".to_string(),
+        issue: 10,
+        topic: "Systems Programming".to_string(),
+    };
+
+    let publications = vec![Publication::Book(book1), Publication::Magazine(magazine1)];
+
+    print_publications(publications);
 }
---
+++ target/debug/deps/assignment_rust_1-1f2065b411848674.d
@@ -0,0 +1,8 @@
+/Users/uygunbodur/Documents/projects/Tutorials/sab-01/rust/assignment-rust-1/target/debug/deps/assignment_rust_1-1f2065b411848674.rmeta: src/main.rs Cargo.toml
+
+/Users/uygunbodur/Documents/projects/Tutorials/sab-01/rust/assignment-rust-1/target/debug/deps/assignment_rust_1-1f2065b411848674.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
